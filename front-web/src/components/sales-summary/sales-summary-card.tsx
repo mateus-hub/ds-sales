2a0b11c43c5289ad6db96aff1d59465fc018dd16
mateus-hub <theusskt2018@gmail.com>
2021-10-22 17:19:36 -0300

    Add SalesSummary component

---
+++ front-web/src/components/sales-summary/sales-summary-card.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './styles.css';
+
+type Props = {
+  value: number;
+  label: string;
+  icon: React.ReactNode;
+};
+
+function SalesSummaryCard({ value, label, icon }: Props) {
+  return (
+    <div className="base-card sales-summary-card">
+      {icon}
+      <h3 className="sales-summary-card-value">{value}</h3>
+      <span className="sales-summary-card-label">{label}</span>
+    </div>
+  );
+}
+
+export default SalesSummaryCard;
