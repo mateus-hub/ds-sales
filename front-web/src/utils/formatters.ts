f025051aef2dbd82be08b9bd64657f99ea4842f7
mateus-hub <theusskt2018@gmail.com>
2021-10-25 11:40:32 -0300

    Integrating Filter to SalesByDate component

--- front-web/src/utils/formatters.ts
+++ front-web/src/utils/formatters.ts
@@ -9,3 +9,9 @@ export const formatPrice = (price: number) => {
 export const formatDate = (data: Date) => {
   return data.toLocaleDateString();
 };
+
+export const formatDateToServer = (date?: Date) => {
+  if (date) {
+    return date?.toISOString().substring(0, 10);
+  }
+};
